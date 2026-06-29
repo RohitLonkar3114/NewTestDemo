8f8e2a70195e61df554ac770af6c680b4f5e5a5e
RohitLonkar3114 <121015204+RohitLonkar3114@users.noreply.github.com>
2026-06-29 20:02:29 +0530

    Add variables and compute sum in Addition class
    Initialize variables for addition and compute the sum.

--- NewTestDemo/src/Addition.java
+++ NewTestDemo/src/Addition.java
@@ -1,6 +1,10 @@
 
 public class Addition {
-
+	 int a=20;
+	 int b=10;
+	 int c;
+     c=a+b;
+     System.out.("Addiiton = "+c);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
