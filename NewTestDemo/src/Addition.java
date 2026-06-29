aa4309bd500acf8f431e9448e7dd32f75bc3c849
RohitLonkar3114 <rohitlonkar3114@gmail.com>
2026-06-29 20:05:40 +0530

    Complete Display Function

--- NewTestDemo/src/Addition.java
+++ NewTestDemo/src/Addition.java
@@ -1,13 +1,16 @@
 
 public class Addition {
-	 int a=20;
-	 int b=10;
-	 int c;
-     c=a+b;
-     System.out.("Addiiton = "+c);
+	void Display()
+	{
+		 int a=20;
+		 int b=10;
+		 int c=a+b;
+	     System.out.println("Addition ="+c);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Addition a1=new Addition();
+		a1.Display();
 	}
 
 }
