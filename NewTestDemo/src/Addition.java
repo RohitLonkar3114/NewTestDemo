2ff1156cc9bb8db849d4bb2aa1a10c824169b3c0
RohitLonkar3114 <121015204+RohitLonkar3114@users.noreply.github.com>
2026-06-29 20:06:55 +0530

    Add Substraction method to Addition class

--- NewTestDemo/src/Addition.java
+++ NewTestDemo/src/Addition.java
@@ -7,6 +7,11 @@ public class Addition {
 		 int c=a+b;
 	     System.out.println("Addition ="+c);
 	}
+
+	void Substraction()
+	{
+		System.out.println("Welcome to Substraction");
+	}
 	public static void main(String[] args) {
 
 		Addition a1=new Addition();
